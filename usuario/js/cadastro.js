f18f361060b0b01c062763d2f224e3fb11acf18f
Natalia Souza <93454080+natsouza24@users.noreply.github.com>
2023-11-07 19:03:40 -0300

    Add files via upload

---
+++ usuario/js/cadastro.js
@@ -0,0 +1,22 @@
+'use static'
+let photo = document.getElementById('imgPhoto-login');
+let file = document.getElementById('flImage-login');
+
+photo.addEventListener("click", () => {
+    file.click();   
+});
+
+file.addEventListener('change', (event) => {
+    
+   if(file.files.length <= 0){
+    return;
+   }
+
+    let reader = new FileReader()
+
+    reader.onload = () =>{
+        photo.src = reader.result;
+    }
+
+    reader.readAsDataURL(file.files[0]);
+});
