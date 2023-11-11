2b59667bbf2a77b3fae8e720510bca2942bd1a55
Bruna <149815631+Bru2005@users.noreply.github.com>
2023-11-11 12:38:50 -0300

    Add files via upload
    São os arquivos com o modal de email da helpet funcionando

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         <link rel="icon" href="images/logo-azul.png">
 
         <!-- Links CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.9.0/dist/sweetalert2.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
@@ -391,7 +392,7 @@
 
             <div class="botoes-contato-anunci">
              <a class="link" href="#fale-conosco">
-                <button name="Enviar">
+                <button name="Enviar" >
                     <span>Contato</span>
                     <!-- <ion-icon name="bookmark-outline"></ion-icon> -->
                 </button>
@@ -418,7 +419,7 @@
                             </p>
                         </div>
             
-                        <form method="POST" action="./forms/contato.php">
+                        <form method="POST" action="./forms/contato.php" id="form">
                             <div class="form-row">
                                 <div class="col">
                                     <input type="text" id="nome" class="form-control" placeholder="Nome" name="Nome">
@@ -429,7 +430,7 @@
                             </div>
                             
                             <div class="form-group">
-                                <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Mensagem" name="Mensagem"></textarea>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Mensagem" rows="4" name="Mensagem"></textarea>
                             </div>
 
                             <div class="botoes-contato">
@@ -444,6 +445,8 @@
                                 </div>
                             </div>
                         </form>
+
+                        <div class="mostrar"></div>
                     </div>
                
                     <div class="secao-img">
@@ -475,9 +478,11 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-        
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.9.0/dist/sweetalert2.all.min.js"></script>
+
         <script src="js/script.js"></script>
         <script src="js/main.js"></script>
+        <script src="js/app.js"></script>
 
     </body>
 </html>
