923bc7b1ae8091b1c3eb9c3cd7f99408e722b96d
Natalia Souza <93454080+natsouza24@users.noreply.github.com>
2023-11-22 22:52:41 -0300

    Update cadastro.js

--- usuario/js/cadastro.js
+++ usuario/js/cadastro.js
@@ -20,3 +20,30 @@ file.addEventListener('change', (event) => {
 
     reader.readAsDataURL(file.files[0]);
 });
+
+//Mostrar senha
+function mostrarSenha(){
+    var inputPass = document.getElementById('senha')
+    var btnShowPass = document.getElementById('btn-senha')
+
+    if(inputPass.type == 'password'){
+        inputPass.setAttribute('type', 'text')
+        btnShowPass.classList.replace('fi-rr-eye', 'fi-rr-eye-crossed')
+    }else{
+        inputPass.setAttribute('type', 'password')
+        btnShowPass.classList.replace('fi-rr-eye-crossed', 'fi-rr-eye')
+    }
+}
+
+function mostrarConfSenha(){
+    var inputPass = document.getElementById('conf-senha')
+    var btnShowPass = document.getElementById('btn-conf-senha')
+
+    if(inputPass.type == 'password'){
+        inputPass.setAttribute('type', 'text')
+        btnShowPass.classList.replace('fi-rr-eye', 'fi-rr-eye-crossed')
+    }else{
+        inputPass.setAttribute('type', 'password')
+        btnShowPass.classList.replace('fi-rr-eye-crossed', 'fi-rr-eye')
+    }
+}
