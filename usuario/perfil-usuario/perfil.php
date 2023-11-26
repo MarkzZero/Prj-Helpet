554abc1fea70beaddb9f8836a50a613040af1248
Natalia Souza <93454080+natsouza24@users.noreply.github.com>
2023-11-26 18:22:16 -0300

    Update front usuário

--- usuario/perfil-usuario/perfil.php
+++ usuario/perfil-usuario/perfil.php
@@ -1,8 +1,5 @@
-
 <?php
-
-include('../config/config.php');
-
+    include('../config/config.php');
 ?>
 
 <!DOCTYPE html>
@@ -15,117 +12,141 @@ include('../config/config.php');
         <link rel="icon" href="images/logo-azul.png">
 
         <!-- Links CSS -->
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/principal.css">
+        <link rel="stylesheet" href="css/modais.css">
+        <link rel="stylesheet" href="css/perfil.css">
+        <link rel="stylesheet" href="css/cards.css">
     </head>
     <body>
-
         <nav>
+            <div class="logo">
+                <img src="images/logo-azul.png">
+            </div>
             <ul>
-                <li><a href="index.php"><img class="img_menu" src="images/home.png"></a></li>
-                <li><a href="notificacoes.php"><img class="img_menu" src="images/noti.png"></a></li>
-                <li><a href="#"><img class="img_menu" src="images/chat.png"></a></li>
-                <li><a href="perfil.php" id="atual"><img class="img_menu" src="images/perfil.png"></a></li>
+                <li><a href="index.php"><i class="fi fi-br-home"></i></a></li>
+                <li><a href="notificacoes.php"><i class="fi fi-br-bell"></i></a></li>
+                <li><a href="Chat.php"><i class="fi fi-br-messages"></i></a></li>
+                <li><a href="perfil.php" id="atual"><i class="fi fi-br-circle-user"></i></a></li>
             </ul>
         </nav>
 
-        <!-- TOPO DO PERFIL -->
-        <div class="topo">
-            <div class="papel-parede">
-                <img src="images/papel-parede-user.png">
-            </div>
+        <div class="container1">
+            <!-- TOPO DO PERFIL -->
+            <div class="topo">
+                <div class="papel-parede">
+                    <img src="images/papel-parede-user.png">
+                </div>
 
-            <div class="area-pesquisa">
-                <div class="icone-config">
-                    <a href="configuracoes.php"><i class="fi fi-sr-settings"></i></a>
+                <div class="area-pesquisa">
+                    <div class="icone-config">
+                        <a href="configuracoes.php"><i class="fi fi-sr-settings"></i></a>
+                    </div>
+                </div>
+
+                <div class="dados-perfil">
+                    <div class="perfil">
+                        <div class="user">
+                            <img src="images/foto-user.png">
+                            <h2><?php echo $_SESSION['nome'];?></h2>
+                            <p>Olá, bem-vindo ao meu perfil!</p>
+                        </div>
+
+                        <div class="user-pref">
+                            <h2 class="titulo-pref">Preferências</h2>
+                            <div class="preferencias">
+                                <div class="coluna">
+                                    <div class="item">
+                                        <img src="images/preferencias/tipo-pet.png">
+                                        <p>Cachorro</p>
+                                    </div>
+                                    <div class="item">
+                                        <img src="images/preferencias/porte.png">
+                                        <p>Porte Médio</p>
+                                    </div>
+                                </div>
+
+                                <div class="coluna">
+                                    <div class="item">
+                                        <img src="images/preferencias/apadrinhamento.png">
+                                        <p>Apadrinhar</p>
+                                    </div>
+                                    <div class="item">
+                                        <img src="images/preferencias/genero.png">
+                                        <p>Fêmea</p>
+                                    </div>
+                                </div>
+
+                                <div class="coluna">
+                                    <div class="item">
+                                        <img src="images/preferencias/idade.png">
+                                        <p>Pet Filhote</p>
+                                    </div>
+                                    <div class="item">
+                                        <img src="images/preferencias/raca.png">
+                                        <p>Sem preferência de raça</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
-            <div class="perfil">
-                <img src="images/foto-user.png">
-                <h2><?php echo $_SESSION['nome'];?></h2>
-                <p>Olá, bem-vindo ao meu perfil!</p>
-                <div class="icons">
-                    <i class="fi fi-rr-share icon-comp"></i>
-                </div>
-            </div>
-        </div>
 
-
-        <!-- PREFERÊNCIAS -->
-        <div class="preferencias">
-            <h2>Preferências</h2>
-            <div class="linha">
-                <div class="item">
-                    <img src="images/preferencias/tipo-pet.png">
-                    <p>Gato</p>
+            <!-- INFORMAÇÕES DO PERFIL -->
+            <div class="informacoes">
+                <!-- Pets Favoritos -->
+                <div id="fav-pets" class="item open-modalFav">
+                    <img class="icon-info" src="images/informacoes/img-fav.png">
+                    <span>Pets Favoritos <br> (7)</span>
+                    <i class="fi fi-br-angle-small-right"></i>
+                    <div class="bolinhas"><img src="images/informacoes/bolinhas-fav.png"></div>
                 </div>
 
-                <div class="item">
-                    <img src="images/preferencias/apadrinhamento.png">
-                    <p>Apadrinhar</p>
+                <!-- Campanhas Favoritas -->
+                <div id="fav-campanhas" class="item open-modalFavCamp">
+                    <img class="icon-info" src="images/informacoes/img-camp.png">
+                    <span>Campanhas Favoritas <br> (4)</span>
+                    <i class="fi fi-br-angle-small-right"></i>
+                    <div class="bolinhas"><img src="images/informacoes/bolinhas-camp.png"></div>
                 </div>
 
-                <div class="item">
-                    <img src="images/preferencias/idade.png">
-                    <p>Pets Filhotes</p>
+                <!-- Pets Anúncios Favoritos -->
+                <div id="fav-anuncios" class="item open-modalFavAnuncios">
+                    <img class="icon-info" src="images/informacoes/img-anuncios.png">
+                    <span>Anúncios Favoritos <br> (3)</span>
+                    <i class="fi fi-br-angle-small-right"></i>
+                    <div class="bolinhas"><img src="images/informacoes/bolinhas-apad.png"></div>
                 </div>
 
-                <div class="item">
-                    <img src="images/preferencias/porte.png">
-                    <p>Porte Médio</p>
+                <!-- Pets Adotados -->
+                <div id="fav-adotados" class="item open-modalAdot">
+                    <img class="icon-info" src="images/informacoes/img-adot.png">
+                    <span>Pets Adotados e Apadrinhados<br> (2)</span>
+                    <i class="fi fi-br-angle-small-right"></i>
+                    <div class="bolinhas"><img src="images/informacoes/bolinhas-adot.png"></div>
                 </div>
 
-                <div class="item">
-                    <img src="images/preferencias/genero.png">
-                    <p>Fêmea</p>
+                <!-- Seguindos ONGs -->
+                <div id="fav-ongs" class="item open-modalSegOng">
+                    <img class="icon-info" src="images/informacoes/img-ongs.png">
+                    <span>Seguindo ONGs e Anunciantes<br> (6)</span>
+                    <i class="fi fi-br-angle-small-right"></i>
+                    <div class="bolinhas"><img src="images/informacoes/bolinhas-ongs.png"></div>
                 </div>
-
-                <div class="item">
-                    <img src="images/preferencias/raca.png">
-                    <p>Sem preferência de raça</p>
-                </div>
-            </div>
-        </div>
-
-
-        <!-- INFORMAÇÕES DO PERFIL -->
-        <div class="informacoes">
-            
-            <!-- Pets Favoritos -->
-            <div id="favoritos" class="item open-modalFav">
-                <img class="icon-info" src="images/informacoes/img-fav.png">
-                <h3>Pets Favoritos <br> (7)</h3>
-                <i class="fi fi-br-angle-small-right"></i>
-                <div class="bolinhas"><img src="images/informacoes/bolinhas-fav.png"></div>
-            </div>
-
-            <!-- Pets Adotados -->
-            <div id="adotados" class="item open-modalAdot">
-                <img class="icon-info" src="images/informacoes/img-adot.png">
-                <h3>Pets Adotados <br> (2)</h3>
-                <i class="fi fi-br-angle-small-right"></i>
-                <div class="bolinhas"><img src="images/informacoes/bolinhas-adot.png"></div>
-            </div>
-
-            <!-- Seguindos ONGs -->
-            <div id="ongs" class="item open-modalSegOng">
-                <img class="icon-info" src="images/informacoes/img-ongs.png">
-                <h3>Seguindo ONGs <br> (6)</h3>
-                <i class="fi fi-br-angle-small-right"></i>
-                <div class="bolinhas"><img src="images/informacoes/bolinhas-ongs.png"></div>
             </div>
 
         </div>
 
         <!-- Modal da Tela de Favoritos -->
-        <div class="fadeFav hide"></div>
         <div class="tela-fav hide">
             <div class="modal-topo">
                 <div class="fechar-modal">
                     <i class="seta fi fi-br-angle-small-left close-modalFav"></i>
                 </div>
 
-                <h2>Favoritos</h2>
+                <h2>Pets Favoritos</h2>
 
                 <div class="imgs-telas">
                     <img src="images/tela-pets-favoritos.png">
@@ -136,75 +157,111 @@ include('../config/config.php');
                 <div class="search">
                     <i class="fi fi-br-search"></i>
                     <input type="text" placeholder="Pesquisar">
-                    <i class="fi fi-rr-settings-sliders"></i>
                 </div>
             </div>
 
-            <?php require "card-pet.php"; ?>
         </div>
 
         <!-- Modal da Tela de Pets Adotados -->
-        <div class="fadeAdot hide"></div>
         <div class="tela-adot hide">
             <div class="modal-topo">
                 <div class="fechar-modal">
                     <i class="seta fi fi-br-angle-small-left close-modalAdot"></i>
                 </div>
 
-                <h2>Pets Adotados</h2>
+                <div class="area-pesquisa">
+                    <h2>Pets Adotados e Apadrinhados</h2>
+                    <div class="search">
+                        <i class="fi fi-br-search"></i>
+                        <input type="text" placeholder="Pesquisar">
+                    </div>
+                </div>
 
                 <div class="imgs-telas">
                     <img src="images/tela-pets-adotados.png">
                 </div>
             </div>
 
-            <div class="area-pesquisa">
-                <div class="search">
-                    <i class="fi fi-br-search"></i>
-                    <input type="text" placeholder="Pesquisar">
-                    <i class="fi fi-rr-settings-sliders"></i>
+        </div>
+
+        <!-- Modal da Tela de Anúncios Favoritos -->
+        <div class="tela-favAnuncios hide">
+            <div class="modal-topo">
+                <div class="fechar-modal">
+                    <i class="seta fi fi-br-angle-small-left close-modalFavAnuncios"></i>
+                </div>
+
+                <div class="area-pesquisa">
+                    <h2>Anúncios Favoritos</h2>
+                    <div class="search">
+                        <i class="fi fi-br-search"></i>
+                        <input type="text" placeholder="Pesquisar">
+                    </div>
+                </div>
+
+                <div class="imgs-telas">
+                    <img src="images/tela-pets-favoritos.png">
                 </div>
             </div>
 
-            <?php require "card-pet.php"; ?>
+        </div>
+
+        <!-- Modal da Tela de Campanhas Favoritas -->
+        <div class="tela-favCamp hide">
+            <div class="modal-topo">
+                <div class="fechar-modal">
+                    <i class="seta fi fi-br-angle-small-left close-modalFavCamp"></i>
+                </div>
+
+                <div class="area-pesquisa">
+                    <h2>Campanhas Favoritas</h2>
+                    <div class="search">
+                        <i class="fi fi-br-search"></i>
+                        <input type="text" placeholder="Pesquisar">
+                    </div>
+                </div>
+
+                <div class="imgs-telas">
+                    <img src="images/tela-pets-favoritos.png">
+                </div>
+            </div>
+
         </div>
 
 
         <!-- Modal da Tela de Seguindos ONGs -->
-        <div class="fadeSegOng hide"></div>
         <div class="tela-segOng hide">
             <div class="modal-topo">
                 <div class="fechar-modal">
                     <i class="seta fi fi-br-angle-small-left close-modalSegOng"></i>
                 </div>
-
-                <h2>Seguidos ONGs</h2>
+                
+                <div class="area-pesquisa">
+                    <h2>Seguidos ONGs e Anunciantes</h2>
+                    <div class="search">
+                        <i class="fi fi-br-search"></i>
+                        <input type="text" placeholder="Pesquisar">
+                    </div>
+                </div>
 
                 <div class="imgs-telas">
-                    <img src="images/tela-seguindo-ongs.png">
+                    <img src="images/tela-pets-favoritos.png">
                 </div>
             </div>
 
-            <div class="area-pesquisa">
-                <div class="search">
-                    <i class="fi fi-br-search"></i>
-                    <input type="text" placeholder="Pesquisar">
-                    <i class="fi fi-rr-settings-sliders"></i>
-                </div>
-            </div>
-
-            <?php require "card-ong.php"; ?>
         </div>
 
-        <!-- SUGESTÕES -->
-        <div class="sugestoes">
-            <h2>Sugestões</h2>
+        <div class="area-cards-user">
             <?php require "card-pet.php"; ?>
         </div>
+        
+        <!-- Sugestões e Categorias -->
+        <?php require "menu-right.php"; ?>
 
 
         <!-- Links JS -->
         <script src="js/script.js"></script>
-        
+        <script src="js/modais.js"></script>
+
     </body>
 </html>
