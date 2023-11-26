86527eede5fa13c5c5c18d0432f348fa5b3310d3
Natalia Souza <93454080+natsouza24@users.noreply.github.com>
2023-11-26 18:19:00 -0300

    Update front usuário

---
+++ usuario/perfil-usuario/post-anuncio.php
@@ -0,0 +1,126 @@
+<div class="post">
+    <div class="area-top">
+        <div class="ong">
+            <img src="images/foto-ong.png">
+            <h4>Anunciante</h4>
+        </div>
+        <div class="icon-fav">
+            <i id="heartIcon1" class="fi-rr-heart icon"></i>
+        </div>
+    </div>
+
+    <div class="campanha">
+        <h4>Nome da Anúncio</h4>
+        <img src="images/foto-ong.png">
+    </div>
+
+    <div class="area-bottom">
+        <p>Descrição</p>
+        <button class="open-modalPerfilAnuncio botao-modal"> 
+            <p>Saiba Mais</p> 
+            <i class="fi fi-br-angle-small-right"></i>
+        </button> 
+    </div>
+
+    <!-- Modal Saiba Mais -->
+    <div class="fadePerfilAnuncio hide"></div>
+    <div class="modalPerfilAnuncio hide">
+        <div class="modal-header">
+            <div class="icon-fav">
+                <i id="heartIcon1" class="fi-rr-heart icon"></i>
+            </div>
+
+            <div class="fechar-modal">
+                <i class="fechar fi fi-br-cross close-modalPerfilAnuncio"></i>
+                <i class="seta fi fi-br-angle-small-left close-modalPerfilAnuncio"></i>
+            </div>
+        </div>
+
+        <div class="modal-info">
+            <div class="info-pet">  
+                <div class="modal-nome">
+                    <div class="foto-modal">
+                        <img src="images/foto-ong.png">
+                    </div>
+                    <div class="nome-pet">
+                        <h3>Anúncio</h3>
+                    </div>
+                </div>
+                
+                <div class="area-itens">
+                    <div class="item">
+                        <h4>Data de Ínicio</h4>
+                        <p>11/05/2023</p>
+                        <div class="icon-patinha">
+                            <i class="fi fi-sr-calendar-clock"></i>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <h4>Data de Término</h4>
+                        <p>30/05/2023</p>
+                        <div class="icon-patinha">
+                            <i class="fi fi-sr-calendar-clock"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="area-descricao">
+                    <div class="post-body">
+                        <span class="short-text">
+                            <p>
+                                A campanha ProAnima tem o objetivo de ajudar todos os animais a tomar
+                                todos as vacinas para evitar alguns problemas de saúde.
+                            </p>
+                            <a class="read-more">Ler mais</a>
+                        </span>
+                        <span class="full-text">
+                            <p>
+                                A campanha ProAnima tem o objetivo de ajudar todos os animais a tomar
+                                todos as vacinas para evitar alguns problemas de saúde.
+                                <a class="read-less">Ler menos</a>
+                            </p>
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="area-ong">
+                <div class="galeria">
+                    <div class="titulo-galeria">
+                        <p>Galeria</p>
+                        <i class="fi fi-br-gallery"></i>
+                    </div>
+                    <div class="fotos">
+                        <img src="images/foto-ong.png">
+                        <img src="images/foto-ong.png">
+                        <img src="images/foto-ong.png">
+                        <img src="images/foto-ong.png">
+                        <img src="images/foto-ong.png">
+                        <img src="images/foto-ong.png">
+                    </div>
+                </div>
+
+                <div class="info-ong">
+                    <img src="images/foto-ong.png">
+
+                    <div class="item-ong">
+                        <div class="nome-ong">
+                            <h3>Nome da Anunciante</h3>
+                            <div class="icons icon-chat">
+                                <i class="fi fi-rr-messages icon-chat"></i>
+                            </div>
+                        </div>
+
+                        <div class="local">
+                            <i class="fi fi-sr-marker"></i>
+                            <p>123 Anywhere St., Any City</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</div>
