cbdc1d264a5ccd4c8ae3e8fc5cf6773cb7423129
Natalia Souza <93454080+natsouza24@users.noreply.github.com>
2023-11-27 23:04:55 -0300

    Update card-ong.php

--- usuario/perfil-usuario/card-ong.php
+++ usuario/perfil-usuario/card-ong.php
@@ -84,23 +84,6 @@
             </div>
 
             <div class="area-campo">
-                <div class="filtros">
-                    <div class="item-filtro atual-filtro">
-                        <img src="images/pet-todos.png">
-                        <p>Todos</p>
-                    </div>
-                    <div class="item-filtro">
-                        <img src="images/pet-gato.png">
-                        <p>Gatos</p>
-                    </div>
-                    <div class="item-filtro">
-                        <img src="images/pet-cao.png">
-                        <p>Cães</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="area-campo">
                 <div class="search">
                     <i class="fi fi-br-search"></i>
                     <input type="text" placeholder="Pesquisar">
