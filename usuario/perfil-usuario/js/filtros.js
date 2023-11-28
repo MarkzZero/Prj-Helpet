0c0a97b67a8cb031b0457a1adb370d154c3e5e7a
Natalia Souza <93454080+natsouza24@users.noreply.github.com>
2023-11-27 23:06:28 -0300

    Add files via upload

---
+++ usuario/perfil-usuario/js/filtros.js
@@ -0,0 +1,15 @@
+/* Exibir Filtros de Pesquisa */
+var acc = document.getElementsByClassName("icon-filtros");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
