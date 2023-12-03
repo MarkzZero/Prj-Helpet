54c6a509a58beef47312df89a3794d8ef138a49e
Natalia Souza <93454080+natsouza24@users.noreply.github.com>
2023-12-03 17:16:38 -0300

    Front atualizado

--- adm/dashboard-adm/menu-lateral.php
+++ adm/dashboard-adm/menu-lateral.php
@@ -12,37 +12,43 @@
     <ul>
         <li>
             <a href="index.php">
-                <span class="icon"><i class="fi fi-sr-chart-line-up"></i></span>
+                <span class="icon"><i class="fi fi-rr-chart-pie-alt"></i></span>
+                <span class="icon icon-hover"><i class="fi fi-sr-chart-pie-alt"></i></span>
                 <span class="title">Dashboard</span>
             </a>
         </li>
         <li>
             <a href="Usuarios.php">
                 <span class="icon"><i class="fi fi-rr-users"></i></span>
+                <span class="icon icon-hover"><i class="fi fi-sr-users"></i></span>
                 <span class="title">Usuários</span>
             </a>
         </li>
         <li>
             <a href="Ongs.php">
                 <span class="icon"><i class="fi fi-rr-house-building"></i></span>
+                <span class="icon icon-hover"><i class="fi fi-sr-house-building"></i></span>
                 <span class="title">ONGs</span>
             </a>
         </li>
         <li>
             <a href="Anunciantes.php">
                 <span class="icon"><i class="fi fi-rr-store-alt"></i></i></span>
+                <span class="icon icon-hover"><i class="fi fi-sr-store-alt"></i></i></span>
                 <span class="title">Anunciantes</span>
             </a>
         </li>
         <li>
             <a href="Chat.php">
                 <span class="icon"><i class="fi fi-rr-messages"></i></span>
+                <span class="icon icon-hover"><i class="fi fi-sr-messages"></i></span>
                 <span class="title">Chat</span>
             </a>
         </li>
         <li>
             <a href="logout.php">
                 <span class="icon"><i class="fi fi-rr-sign-out-alt"></i></span>
+                <span class="icon icon-hover"><i class="fi fi-br-sign-out-alt"></i></span>
                 <span class="title">Sair</span>
             </a>
         </li>
