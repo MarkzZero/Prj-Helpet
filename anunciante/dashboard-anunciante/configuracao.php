4787ce994bbab1c4cf135907b20c5add04d340b6
Natalia Souza <93454080+natsouza24@users.noreply.github.com>
2023-12-04 15:52:20 -0300

    Front Anunciante

--- anunciante/dashboard-anunciante/configuracao.php
+++ anunciante/dashboard-anunciante/configuracao.php
@@ -18,13 +18,12 @@
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
         <link rel="stylesheet" href="css/modais.css">
+        <link rel="stylesheet" href="css/configuracao.css">
     </head>
     <body>
         
         <!-- Menu Fixo Lateral -->
         <?php require "menu-lateral.php"; ?>
-
-       
 
         <div class="main">
             <div class="topbar">
@@ -35,48 +34,33 @@
                 </div>
             </div>
 
-               <div class="menu-config">
-
-                    <div class="titulo-config">
-                        <span>Configurações</span>
-                    </div>
-
-                    <div class="area-botao">
-
-                        <hr>
-
-                        <div class="botao">
-                            <i class="bi bi-arrow-left-right"></i>
-                            <a href="edit-anunciante.php">
-                                Alterar Perfil
-                            </a>
-                        </div>
-
-                        <hr>
-
-                        <div class="botao open-modalExc">
-                        <i class="bi bi-trash3-fill"></i>
-                            <a>
-                                Excluir Perfil
-                            </a>
-                        </div>
-
-                        <hr>
-
-
-                    </div>
-
-                    <!-- MODO ESCURO E CLARO -->
-                    <div class="btn-modo">
-                        <i class="fi fi-rs-moon-stars"></i>
-                        <p>Modo Escuro</p>
-                        <input type="checkbox" id="switch" /><label for="switch"></label>
-                    </div>
-
+            <div class="menu-config">
+                <div class="titulo-config">
+                    <span>Configurações</span>
                 </div>
 
+                <div class="area-botao">
+                    <hr>
+                    
+                    <a href="edit-anunciante.php">
+                        <div class="botao">
+                            <i class="fi fi-br-arrows-repeat"></i>
+                            <p>Alterar Perfil</p>
+                        </div>
+                    </a>
 
-          
+                    <hr>
+
+                    <div class="botao open-modalExc">
+                        <i class="fi fi-br-trash"></i>
+                        <p>Excluir Perfil</p>
+                    </div>
+
+                    <!-- Excluir Anunciante -->
+                    <?php require "excluir-anunciante.php"; ?>
+                    <hr>
+                </div>
+            </div>
         </div>
 
         <!-- Links JS -->
