9880bc75abae0c58b3c7b410f94b56e2e3bfe55c
Bruna <149815631+Bru2005@users.noreply.github.com>
2023-12-06 14:42:56 -0300

    Add files via upload

--- anunciante/js/script.js
+++ anunciante/js/script.js
@@ -79,3 +79,36 @@ function pesquisacep(valor) {
     
       input.value = cep;
     };
+
+    // VALIDAÇÃO CNPJ
+    function formatCNPJ(input) {
+      var cnpj = input.value.replace(/\D/g, '');
+  
+      if (cnpj.length > 2) {
+          cnpj = cnpj.substr(0, 2) + '.' + cnpj.substr(2);
+      }
+      if (cnpj.length > 6) {
+          cnpj = cnpj.substr(0, 6) + '.' + cnpj.substr(6);
+      }
+      if (cnpj.length > 10) {
+          cnpj = cnpj.substr(0, 10) + '/' + cnpj.substr(10);
+      }
+      if (cnpj.length > 15) {
+          cnpj = cnpj.substr(0, 15) + '-' + cnpj.substr(15);
+      }
+  
+      input.value = cnpj;
+  }
+
+  // FORMATAÇÃO TELEFONE
+  function formatTelefone(input) {
+    var phoneNumber = input.value.replace(/\D/g, ''); // Remove todos os não dígitos
+  
+    if (phoneNumber.length === 11) {
+        phoneNumber = phoneNumber.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3');
+    } else {
+        // Mantém o número como está, caso não se encaixe no formato esperado
+    }
+  
+    input.value = phoneNumber;
+  }
