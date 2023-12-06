008601708999121007026c7c1c58f2a6f881dbb0
Natalia Souza <93454080+natsouza24@users.noreply.github.com>
2023-12-06 14:51:55 -0300

    Add files via upload

---
+++ js/splash.js
@@ -0,0 +1,8 @@
+/* TELA DE SPLASH */
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+  setTimeout(()=>{
+    splash.classList.add('display-none');
+  }, 2000);
+})
